5c11e03540ab67a4c37ce8e382194f249749e0ce
Adriano Bianchini <animationbureau@gmail.com>
2012-04-09 18:18:41 -0700

    Icons and stuff

--- Social Calc/src/com/advancementbureau/socialcalc/SocialCalcActivity.java
+++ Social Calc/src/com/advancementbureau/socialcalc/SocialCalcActivity.java
@@ -1,7 +1,10 @@
 package com.advancementbureau.socialcalc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SocialCalcActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,23 @@ public class SocialCalcActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	getMenuInflater().inflate(R.menu.mainoptions, menu);
+    	menu.findItem(R.id.help_menu_item).setIntent(new Intent(this, HelpActivity.class));
+    	menu.findItem(R.id.settings_menu_item).setIntent(new Intent(this, SettingsActivity.class));
+    	return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	super.onOptionsItemSelected(item);
+    	if (item.getItemId() == R.id.settings_menu_item) {
+    			startActivity(item.getIntent()); }
+    	if (item.getItemId() == R.id.help_menu_item) {
+			startActivity(item.getIntent()); }
+    	return true;
     }
 }
